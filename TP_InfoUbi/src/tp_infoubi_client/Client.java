cbf5ddacdcfc24ba71c4eb7dcb0a2364e4187198
jordane-quincy <jordanequincy@gmail.com>
2016-04-28 21:12:56 +0200

    quand on ne met pas le string buffer au bon endroit aussi ...

--- TP_InfoUbi/src/tp_infoubi_client/Client.java
+++ TP_InfoUbi/src/tp_infoubi_client/Client.java
@@ -25,26 +25,46 @@ public class Client {
 	public void bindCapteurHumiditeService(final CapteurHumidite service,
 			final Map<String, String> props) {
 		synchronized (localisationServices) {
-			System.out.println("IN - bindCapteurHumiditeService");
 			final String localisationDuService = props.get("localisation");
+			System.out.println("bind Capteur Humidite : "
+					+ service.getClass().getName() + ", pros : "
+					+ props.toString());
 
-			final Map<Object, Map<String, String>> serviceInfosMap = new HashMap<Object, Map<String, String>>();
+			final Map<Object, Map<String, String>> serviceInfosMap = localisationServices
+					.get(localisationDuService) == null ? new HashMap<Object, Map<String, String>>()
+					: localisationServices.get(localisationDuService);
 			serviceInfosMap.put(service, props);
 
+			System.out.println("avant ajout : " + localisationServices.size()
+					+ ", " + serviceInfosMap.size());
 			localisationServices.put(localisationDuService, serviceInfosMap);
+			System.out.println("apres ajout : " + localisationServices.size()
+					+ ", " + serviceInfosMap.size());
 		}
 	}
 
 	public void bindCapteurTemperatureService(final CapteurTemperature service,
 			final Map<String, String> props) {
 		synchronized (localisationServices) {
-			System.out.println("IN - bindCapteurTemperatureService");
+			System.out.println("bind Capteur Temperature : "
+					+ service.getClass().getName() + ", pros : "
+					+ props.toString() + ", avant ajout : "
+					+ localisationServices.size());
+
 			final String localisationDuService = props.get("localisation");
 
-			final Map<Object, Map<String, String>> serviceInfosMap = new HashMap<Object, Map<String, String>>();
+			final Map<Object, Map<String, String>> serviceInfosMap = localisationServices
+					.get(localisationDuService) == null ? new HashMap<Object, Map<String, String>>()
+					: localisationServices.get(localisationDuService);
+			System.out.println("serviceInfosMap avant : " + serviceInfosMap);
 			serviceInfosMap.put(service, props);
+			System.out.println("serviceInfosMap apres : " + serviceInfosMap);
 
+			System.out.println("avant ajout : " + localisationServices.size()
+					+ ", " + serviceInfosMap.size());
 			localisationServices.put(localisationDuService, serviceInfosMap);
+			System.out.println("apres ajout : " + localisationServices.size()
+					+ ", " + serviceInfosMap.size());
 		}
 	}
 
@@ -86,17 +106,18 @@ public class Client {
 			public void run() {
 
 				while (go) {
-					System.out.println("taille service dispo : "
-							+ localisationServices.size());
+					System.out
+							.println("nb localisation avec au moins un capteur : "
+									+ localisationServices.size());
 
-					final StringBuffer buff = new StringBuffer();
 					for (final String localisationString : localisationServices
 							.keySet()) {
+						final StringBuffer buffLocalisation = new StringBuffer();
 						final Map<Object, Map<String, String>> mapServicesDispoDansCettePiece = localisationServices
 								.get(localisationString);
 
-						System.out
-								.println("Dans '" + localisationString + "' ");
+						buffLocalisation.append("Dans '" + localisationString
+								+ "' : \n");
 						for (final Object serviceObject : mapServicesDispoDansCettePiece
 								.keySet()) {
 							final Map<String, String> serviceProps = mapServicesDispoDansCettePiece
@@ -104,23 +125,28 @@ public class Client {
 
 							if (serviceObject instanceof CapteurHumidite) {
 								final CapteurHumidite capteurHumidite = (CapteurHumidite) serviceObject;
-								System.out.println("le taux d'humidité est de "
-										+ capteurHumidite.getHumidite()
-										+ serviceProps.get("unite"));
+								buffLocalisation
+										.append("- le taux d'humidité est de "
+												+ capteurHumidite.getHumidite()
+												+ serviceProps.get("unite"));
 							} else if (serviceObject instanceof CapteurTemperature) {
 								final CapteurTemperature capteurTemperature = (CapteurTemperature) serviceObject;
-								System.out.println("il fait "
+								buffLocalisation.append("- il fait "
 										+ capteurTemperature.getTemp() + "°"
 										+ serviceProps.get("unite"));
 							} else {
-								System.out.println("serviceObject class : "
+								System.err.println("serviceObject class : "
 										+ serviceObject.getClass());
 							}
+
+							buffLocalisation.append(".\n");
 						}
+
+						System.out.println(buffLocalisation.toString());
 					}
 
 					try {
-						Thread.sleep(3000); // on check toutes les 3 secondes
+						Thread.sleep(10000); // on check toutes les 3 secondes
 					} catch (final InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
